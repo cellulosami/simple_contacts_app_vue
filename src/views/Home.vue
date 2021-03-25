ce01e90706cfa559ed49d078afc9c58c8833cf03
austin shields <ajshields196@gmail.com>
2021-03-24 22:19:49 -0400

    add show and update functions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,18 +8,49 @@
     <p> Phone Number: <input v-model="params.phone_number"></p>
     <p> Image URL: <input v-model="params.image"></p>
     <button v-on:click="contactsCreate">Create</button>
-    {{ params }}
+    <br>
+    <br>
+    <br>
     <hr>
+    <br>
     <div v-for="contact in contacts" v-bind:key="contact.id">
-      <p>{{ contact.first_name }} {{ contact.last_name }}</p>
+      <p>{{ contact.first_name }} {{ contact.last_name }} <button v-on:click="contactsShow(contact)">More info</button></p>
+      
       <img v-bind:src="contact.image" style="width: 200px;">
+      <br>
+      <br>
+      <br>
+      <br>
     </div>
+    <dialog id="contact-details">
+      <form method="dialog">
+        <p>First Name: <input v-model="currentContact.first_name"></p>
+        <p>Last Name: <input v-model="currentContact.last_name"></p>
+        <p>Email: <input v-model="currentContact.email"></p>
+        <p>Phone Number: <input v-model="currentContact.phone_number"></p>
+        <p>Image URL: <input v-model="currentContact.image"></p>
+        <button>Close</button>
+        <button v-on:click="contactsUpdate">Update</button>
+      </form>
+    </dialog>
   </div>
 </template>
  
 <style>
 p {
   font-weight: bold;
+}
+
+button {
+  background-color: rgb(233, 221, 195);
+}
+
+dialog {
+  text-align: left;
+}
+
+input {
+  width: 400px;
 }
 </style>
 
@@ -37,7 +68,8 @@ export default {
         email: "",
         phone_number: "",
         image: ""
-      }
+      },
+      currentContact: {}
     };
   },
   created: function() {
@@ -60,6 +92,19 @@ export default {
         .then(response => {
           this.contacts.push(response.data);
         });
+    },
+    contactsShow: function (contact) {
+      console.log(contact);
+      this.currentContact = contact
+      document.querySelector("#contact-details").showModal();
+    },
+    contactsUpdate: function () {
+      console.log("updating...");
+      axios
+        .patch("/api/contacts/" + this.currentContact.id, this.currentContact)
+        .then(response => {
+          console.log(response.data);
+        })
     }
   }
 };
